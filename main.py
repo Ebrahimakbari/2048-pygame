50459ec94c6207df36f1d3fa595723516c7e2eb9
Ebrahim_ak <y560mia3@gmail.com>
2024-07-02 18:56:57 +0330

    draw_line_rect

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ FPS = 60
 WIDTH, HEIGHT = 800, 800
 ROWS = 4
 COLUMNS = 4
-RECT_HEIGHT = WIDTH // COLUMNS
+RECT_WIDTH = WIDTH // COLUMNS
 RECT_HEIGHT = HEIGHT // ROWS
 OUTLINE_COLOR = (187, 173 , 160)
 OUTLINE_THICKNESS = 10
@@ -18,6 +18,22 @@ FONT = pygame.font.SysFont('comicsans', 60, bold=True)
 MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((HEIGHT,WIDTH))
 
+def draw_grid(window):
+    for row in range(1,ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window,OUTLINE_COLOR, (0,y), (WIDTH,y), OUTLINE_THICKNESS )
+
+    for column in range(1,COLUMNS):
+        x = column * RECT_WIDTH
+        pygame.draw.line(window,OUTLINE_COLOR,(x,0),(x,HEIGHT),OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0,0,WIDTH,HEIGHT), OUTLINE_THICKNESS)
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+    draw_grid(window)
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
     run = True
@@ -28,6 +44,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw(window)
+
     pygame.quit()
 
 if __name__ == '__main__':
