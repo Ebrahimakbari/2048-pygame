342a16521548e7f180e42309ce7cd65e175228ab
Ebrahim_ak <y560mia3@gmail.com>
2024-07-03 19:57:04 +0330

    added left movement

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ BACKGROUND_COLOR = (205, 192 , 180)
 FONT_COLOR = (119, 110 , 101)
 
 pygame.display.set_caption('2048')
-FONT = pygame.font.SysFont('comicsans', 60, bold=True)
+FONT = pygame.font.SysFont('consolas', 60, bold=True)
 MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((HEIGHT,WIDTH))
 
@@ -54,8 +54,80 @@ class Tile:
             (
                 self.x + (RECT_WIDTH/2 - text.get_width()/2),
                 self.y + (RECT_HEIGHT/2 - text.get_height()/2)
+            )
         )
+    def set_pos(self, ceil=False):
+        if ceil:
+            self.row = math.ceil(self.y / RECT_HEIGHT)
+            self.col = math.ceil(self.x / RECT_WIDTH)
+        else:
+            self.row = math.floor(self.y / RECT_HEIGHT)
+            self.col = math.floor(self.x / RECT_WIDTH)
+    
+    def move(self, delta):
+        self.x += delta[0]
+        self.y += delta[1]
+
+
+def end_move(tiles):
+    if len(tiles) == 16:
+        return "lost"
+    row,col = get_random_pos(tiles)
+    tiles[f'{row}{col}'] = Tile(random.choice([2,4]), row, col)
+    return 'continue'
+
+def update_tiles(window,tiles,sorted_tiles):
+    tiles.clear()
+    for tile in sorted_tiles:
+        tiles[f'{tile.row}{tile.col}'] = tile
+        
+    draw(window,tiles)
+
+def move_tiles(window,tiles,clock,direction):
+    updated = True
+    blocks = set()
+
+    if direction == 'left':
+        sort_func = lambda x: x.col
+        reverse = False
+        delta = (-MOVE_VEL,0)
+        boundary_check = lambda tile:tile.col == 0
+        get_next_tile = lambda tile: tiles.get(f'{tile.row}{tile.col - 1}')
+        merge_check = lambda tile, next_tile:tile.x > next_tile.x + MOVE_VEL
+        move_check = (
+            lambda tile, next_tile:tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
         )
+        ceil = True
+    
+    while updated:
+        clock.tick(FPS)
+        updated = False
+        sorted_tiles = sorted(tiles.values(), key=sort_func, reverse=reverse)
+        for i,tile in enumerate(sorted_tiles):
+            if boundary_check(tile):
+                continue
+            next_tile = get_next_tile(tile)
+            if not next_tile:
+                tile.move(delta)
+            elif (
+                tile.value == next_tile.value
+                and tile not in blocks
+                and next_tile not in blocks
+            ):
+                if merge_check(tile,next_tile):
+                    tile.move(delta)
+                else:
+                    next_tile.value += 2
+                    sorted_tiles.pop(i)
+                    blocks.add(next_tile)
+            elif move_check(tile, next_tile):
+                tile.move(delta)
+            else:
+                continue
+            tile.set_pos(ceil)
+            updated = True
+        update_tiles(window,tiles ,sorted_tiles)
+    return end_move(tiles)
 
 
 def get_random_pos(tiles):
@@ -108,6 +180,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    move_tiles(window,tiles,clock,'left')
         draw(window,tiles)
 
     pygame.quit()
